97b3538736fd04c0ca76d85a294366d97e330b91
wakasugi <wshun1222@gmail.com>
2019-12-02 15:08:32 +0900

    update nas.py

--- src/nas.py
+++ src/nas.py
@@ -2,10 +2,12 @@ def nas(args, task, preprocess_func):
     ''' Network Architecture Search method
 
     Given task and preprocess function, this method returns a model output by NAS.
-
+    
+    The implementation of DARTS is available at https://github.com/alphadl/darts.pytorch1.1 
     '''
 
-    # TODO: Implement NAS
-    model = None
+    # TODO: Replace model with the output by NAS
+    model = torch.nn.Linear(?, task.n_classes)
 
+    # return a neural network model (torch.nn.Module)
     return model
